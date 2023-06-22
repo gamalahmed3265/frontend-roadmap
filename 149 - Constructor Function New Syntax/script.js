df631e95a6795164c0b6b92c6ef9619a81b88c9f
gamal ahmed <gamalahmed3265@gmail.com>
2023-06-22 11:00:10 +0300

    add 149 - Constructor Function New Syntax

---
+++ 149 - Constructor Function New Syntax/script.js
@@ -0,0 +1,17 @@
+class User{
+constructor (id,name,salary){
+  this.id=id;
+  this.name=name;
+  this.salary=salary;
+  }
+}
+let u1=new User(100,"gamal",2000);
+let u2=new User(10,"ahmed",2100);
+
+console.log(u1.id);
+console.log(u1.name);
+console.log(u1.salary);
+
+console.log(u2.id);
+console.log(u2.name);
+console.log(u2.salary);
